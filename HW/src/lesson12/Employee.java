b0984fa4dab6dd9be23718489a3d886421dafb04
Dima <74459076+diamam90@users.noreply.github.com>
2021-01-27 00:00:47 +0300

    111

---
+++ HW/src/lesson12/Employee.java
@@ -0,0 +1,32 @@
+package lesson12;
+
+public class Employee {
+	private String fullName;
+	private int salary;
+	public Employee(String fullName, int salary) {
+		this(fullName);
+		this.salary=salary;
+	}
+	public Employee(String fullName) {
+		this.fullName=fullName;
+	}
+	public String getFullName() {return fullName;}
+	public int getSalary() {return salary;}
+	public void setFullName(String name) {fullName=name;}
+	public void setSalary(int salary) {this.salary=salary;}
+	public static void main(String[] args) {
+		Employee emp1=new Employee("Иванов И.И", 36000);
+		Employee emp2=new Employee("Сидоров С.С", 106000);
+		Employee emp3=new Employee("Иванова А.А", 16000);
+		Employee emp4=new Employee("Ива F.F", 6000);
+		Employee[] employees = {emp1,emp2,emp3,emp4, new Employee("Швалова Ш.А.", 40000)};
+		Report.generationReport(employees);
+	}
+}
+class Report{
+	public static void generationReport(Employee... emp) {
+		for (Employee e:emp) {
+		System.out.printf("ФИО сотрудника:%-15s, заработная плата:%,9d\n",e.getFullName(),e.getSalary());
+		}
+	}
+}
