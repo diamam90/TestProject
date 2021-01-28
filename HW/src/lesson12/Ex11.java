e1eccdad72f4e8c771534829357f689e113ad5dd
Dima <74459076+diamam90@users.noreply.github.com>
2021-01-28 23:19:57 +0300

    done

--- HW/src/lesson12/Ex11.java
+++ HW/src/lesson12/Ex11.java
@@ -12,57 +12,59 @@ public class Ex11 {
 			char[] chs = strings[i].toCharArray();
 			count[i] = 1;
 			Arrays.sort(chs);
-			for (int j =0; j<chs.length-1;j++) {
-				if (chs[j]!=chs[j+1]) {count[i]=count[i]+1;}
+			for (int j = 0; j < chs.length - 1; j++) {
+				if (chs[j] != chs[j + 1]) {
+					count[i] = count[i] + 1;
+				}
 			}
-			
-			
-		//	System.out.println(count[i]);
+
+			// System.out.println(count[i]);
 		}
-		int min=count[0];
-		int pos=0;
-		for (int i=0; i< strings.length;i++) {
-			if (count[i]<min) {
-				min=count[i];
-				pos=i;
+		int min = count[0];
+		int pos = 0;
+		for (int i = 0; i < strings.length; i++) {
+			if (count[i] < min) {
+				min = count[i];
+				pos = i;
 			}
 		}
-		System.out.println("Cлово (" + strings[pos]+ 
-				") с минимальным количеством разных символов (" + min+ ")");
+		System.out.println("Cлово (" + strings[pos] + ") с минимальным количеством разных символов (" + min + ")");
 	}
+
 	public static void countOfLatina(String str) {
-		
-		Pattern p= Pattern.compile("[a-zA-Z]\\W");
-		Matcher m=p.matcher(str);
-		int count=0;
+
+		Pattern p = Pattern.compile("[a-zA-Z]\\W");
+		Matcher m = p.matcher(str);
+		int count = 0;
 		while (m.find()) {
 			count++;
 		}
-		System.out.println("Количество слов, содержащих"
-				+ " только латинские буквы - " + count);
+		System.out.println("Количество слов, содержащих" + " только латинские буквы - " + count);
 	}
+
 	public static void palindrome(String s) {
-		Matcher m= Pattern.compile("\\w{2,}").matcher(s);
-		
+		Matcher m = Pattern.compile("\\w{2,}").matcher(s);
+		String str = "";
 		while (m.find()) {
-			System.out.println(m.group());
-			StringBuilder str1 = new StringBuilder();
-			StringBuilder str2 = new StringBuilder();
-			str1.append(m.group());
-			str2.append(m.group()).reverse();
-			System.out.println(str1!=str2);
+			char[] chs = m.group().toCharArray();
+			int count = 0;
+			for (int i = 0; i < chs.length; i++) {
+				if (chs[i] == chs[chs.length - i - 1]) {
+					count++;
+				}
 			}
-//			
-			
+			if (count == chs.length) {
+				System.out.println(m.group() + " палиндром");
+			}
 		}
-		
 	}
+
 	public static void main(String[] args) {
-//		String str = "fffffac ab fb 1234 jkjk";
-//		minDifferentChar(str);
-//		String s="One two three раз два три one1 two2 123 ";
-//		countOfLatina(s);
-		String s1="Если есть хвосты по дз, начните с 1 не сданного задания. 123 324 111 4554";
+		String str = "fffffac ab fb 1234 jkjk";
+		minDifferentChar(str);
+		String s = "One two three раз два три one1 two2 123 ";
+		countOfLatina(s);
+		String s1 = "Если есть хвосты по дз, начните с 1 не сданного задания. 123 324 111 4554";
 		palindrome(s1);
 	}
 }
