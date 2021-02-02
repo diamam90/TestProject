1a075289cead9454b0f3e66210fddd65c88f0af8
Dima <74459076+diamam90@users.noreply.github.com>
2021-02-02 22:10:38 +0300

    well done

--- HW/src/lesson12/Shop.java
+++ HW/src/lesson12/Shop.java
@@ -41,7 +41,6 @@ class User {
 class Category {
 	private static int count;
 	private int id;
-	private int productId[];
 	Products[] product;
 	private String name;
 
@@ -49,6 +48,7 @@ class Category {
 		id = ++count;
 		this.name = name;
 		this.product = product;
+		
 	}
 
 	public String getName() {
@@ -126,14 +126,10 @@ class Products {
 	private double rate;
 	private double price;
 
-	Products(String name, double price) {
+	Products(String name, double price, String manufacturer, double rate) {
 		id = ++count;
 		this.price = price;
 		this.name = name;
-	}
-
-	Products(String name, double price, String manufacturer, double rate) {
-		this(name, price);
 		this.rate = rate;
 		this.manufacturer = manufacturer;
 	}
@@ -176,8 +172,7 @@ class Products {
 
 	@Override
 	public String toString() {
-		return  "[" + name + ", производитель = " + manufacturer + ", рейтинг = " + rate + ", цена = " + price
-				+ "]";
+		return  String.format("[%-20s, производитель = %-20s, рейтинг = %-3s, цена = %-6.2f] \n",name,manufacturer,rate,price);
 	}
 
 }
@@ -187,11 +182,11 @@ public class Shop {
 	public static void main(String[] args) {
 		User user1 = new User("andi", "mpei34525");
 		user1.setName("Дмитрий");
-		Products product1 = new Products("Домик в деревне 3.2% ", 60);
-		Products product2 = new Products("Веселый молочник 3.2% ", 50);
-		Products product3 = new Products("Волжаночка 2.5%", 50, "Волжский молочный завод", 4.6);
+		Products product1 = new Products("Домик в деревне 3.2",60,"проклятый старый дом",4.4 );
+		Products product2 = new Products("Веселый молочник 3.2",50,"Молочник на веселе ", 5);
+		Products product3 = new Products("Волжаночка 2.5", 50, "Волжский молочный завод", 4.6);
 
-		Products product4 = new Products("Мясо птицы", 200);
+		Products product4 = new Products("Мясо птицы", 200, "птицемяс", 3.22);
 		Products product5 = new Products("Фуагра", 1000, "завод мертвых тушек", 5);
 		Products product6 = new Products("Мраморная говядина", 800, "Фрезерный цех", 4.8);
 
@@ -202,9 +197,9 @@ public class Shop {
 
 		Products product11 = new Products("Очаково", 55, "Очаковский завод пива ", 3.3);
 		Products product12 = new Products("Corona Extra", 80, "Мексика, отвечаю", 5);
-		Products product13 = new Products("Miller", 65);
+		Products product13 = new Products("Miller", 65, "экспортное", 4.8);
 
-		Products product14 = new Products("Волжанин", 30);
+		Products product14 = new Products("Волжанин", 30, "тут все понятно", 2.2);
 		Products product15 = new Products("Пегас", 30, "ЗЛК", 2.4);
 		Products product16 = new Products("Chappi", 80, "Chappi corp.", 4.4);
 
@@ -217,32 +212,35 @@ public class Shop {
 		Products[] products = { product1, product2, product3, product4, product5, product6, product7, product8,
 				product9, product10, product11, product12, product13, product14, product15, product16 };
 		Category[] categories = { meat, milk, beer, forAnimal, pasta };
-		// checkLogin(user1);
-		
-		for (Category c : categories) {
-			System.out.print(c.getId() + "  " + c.getName() + "  ");
+		if (checkLogin(user1)) {
+			Cart cart1 = new Cart();
+			for (int i=0;i<3;i++) {
+				System.out.println("Выберите категорию:");
+				for (Category c : categories) {
+				System.out.print(c.getId() + "  " + c.getName() + "  ");
+				}
+				System.out.println();
+				
+				int cat=inputCheck(0, categories.length); 
+			
+				System.out.println("Категория "+cat +": " + categories[cat - 1].getName());
+				System.out.println("Выберите товар:");
+				int k=1;
+				for (Products p : categories[cat - 1].getAllProducts()) {
+					System.out.printf(k + " "+p.toString());
+					k++;
+				}
+				int prod=inputCheck(0,categories[cat-1].product.length);
+				System.out.println("Вы выбрали: " + " "+ categories[cat-1].getProduct(prod-1));
+				System.out.println("Cколько штук положить в корзину?");
+				int n=inputCheck(0,100);
+				
+				cart1.addProduct(categories[cat-1].getProduct(prod-1),n);
+			}
+			cart1.showCart();
+			System.out.println("Итого: " + cart1.getTotalCost() + " руб.");
 		}
-		System.out.println();
 		
-		int cat=4;
-//		cat=inputCheck(0, categories.length); 
-		
-		System.out.println("Категория "+cat +": " + categories[cat - 1].getName());
-		System.out.println("Выберите товар:");
-		for (Products p : categories[cat - 1].getAllProducts()) {
-			System.out.println(p.toString());
-		}
-		//cat = 2;
-		System.out.println("Вы выбрали: " + " "+ categories[cat-1].getProduct(1) + ", сколько штук положить в корзину?");
-//		int p1 = 14;
-//		int n1 = 2;
-//		int p2 = 6;
-//		int n2 = 3;
-//		Cart cart1 = new Cart();
-//		cart1.addProduct(products[p1], n1);
-//		cart1.addProduct(products[p2], n2);
-//		System.out.println("Итого: " + cart1.getTotalCost() + " руб.");
-//		cart1.showCart();
 	}
 	
 	public static int inputCheck(int start, int end) {
@@ -251,17 +249,19 @@ public class Shop {
 		Scanner s = new Scanner(System.in);
 		if (s.hasNextInt()) {
 			choice = s.nextInt();
-			if ((choice<start) | (choice>end)) {
+			if ((choice<=start) | (choice>end)) {
 				System.out.println("Введено некорректное значение, программа автоматически сделала выбор");
 				choice=1;
 			}
 		return choice;
 		}
-		else return 1;
+		else {System.out.println("Введено некорректное значение, программа автоматически сделала выбор");
+			return 1;
+		}
 		
 }
 	
-	public static void checkLogin(User user) {
+	public static boolean checkLogin(User user) {
 		System.out.println("Приветствуем Вас в нашем магазине!");
 		System.out.println("Введите логин:");
 		Scanner scan1 = new Scanner(System.in);
@@ -272,9 +272,12 @@ public class Shop {
 			String p = scan2.nextLine();
 			if (l.equals(user.getLogin()) && (p.equals(user.getPassword()))) {
 				System.out.println("Добро пожаловать, " + user.getName());
+				return true;
 			} else {
 				System.out.println("Логин и пароль не соответсвуют");
+				return false;
 			}
 		}
+		else return false;
 	}
 }
