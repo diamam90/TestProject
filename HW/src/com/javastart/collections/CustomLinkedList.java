154b0e19a7dbd9bda3d4b3302479d39eaf4c747e
Dima <74459076+diamam90@users.noreply.github.com>
2021-02-23 21:04:12 +0300

    111

--- HW/src/com/javastart/collections/CustomLinkedList.java
+++ HW/src/com/javastart/collections/CustomLinkedList.java
@@ -3,11 +3,55 @@ package com.javastart.collections;
 public class CustomLinkedList<Element> {
 	private Node<Element> first;
 	private Node<Element> last;
+	
+	private int size;
+	
+	public void add(Element element) {
+		if (first == null) {
+			 Node<Element> node = new Node<>(element,null,null);
+			 first = node;
+			 last = node;
+		} else {
+			Node<Element> node = new Node<>(element, null, last);
+			last.next = node;
+			last = node; 
+		}
+		size++;
+	}
+	public void addFirst(Element element) {
+		
+		Node<Element> newFirstElement = new Node<>(element,first,null);
+		first.prev=newFirstElement;
+		first=newFirstElement;
+		size++;
+ 	}
+	public void delete(int index) {
+		Node<Element> deleteNode=getNodeByIndex(index);
+		Node<Element> prevDN=deleteNode.prev;
+		Node<Element> nextDN=deleteNode.next;
+		prevDN.next=nextDN;
+		nextDN.prev=prevDN;
+		size--;
+		
+	}
+	public Node<Element> getNodeByIndex(int index){
+		Node<Element> result=first;
+		for (int i=0; i<index;i++) {
+			 result=result.next;
+		}
+		return result;
+	}
+	public Element first() {return first.item;}
+	public Element last() {return last.item;}
+	
+	public Element get(int index){
+		return getNodeByIndex(index).item;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-	private class Node<Element>{
+	private static class Node<Element>{
 		Element item;
 		Node<Element> next;
 		Node<Element> prev;
@@ -17,5 +61,8 @@ public class CustomLinkedList<Element> {
 			this.prev = prev;
 		}
 	
+	}
+	public int size() {
+		return size;
 	}
 }
---
+++ HW/src/lesson14/Class1.java
@@ -0,0 +1,24 @@
+package lesson14;
+
+import com.javastart.collections.CustomLinkedList;
+
+public class Class1 {
+	public static void main(String[] args) {
+		CustomLinkedList<String> cll1= new CustomLinkedList<>();
+		cll1.add("SALAM ALEIKUM!");
+		cll1.add("VALEIKUM ASSALAM!");
+		cll1.add("Shalom");
+		cll1.add("Shalom");
+		cll1.addFirst("AZAZA");
+		cll1.addFirst("ASSA");
+		
+		for (int i=0;i<cll1.size();i++) {
+			System.out.print(cll1.get(i)+ " ");
+		}
+		System.out.println();
+		cll1.delete(2);
+		for (int i=0;i<cll1.size();i++) {
+			System.out.print(cll1.get(i)+ " ");
+		}
+}
+}
